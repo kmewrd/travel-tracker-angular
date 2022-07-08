e0a74ad44492c2ae209a6f26dba975ee487d95d7
Kim Ward <kmewrd@outlook.com>
2022-07-08 15:14:25 -0500

    Add form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BookingFormComponent } from './booking-form/booking-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingFormComponent
   ],
   imports: [
     BrowserModule
