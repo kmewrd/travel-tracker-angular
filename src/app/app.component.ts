b5fbb3e020076d4f88f8e06934f8c9234b922a93
Kim Ward <kmewrd@outlook.com>
2022-07-08 15:52:40 -0500

    Modify traveler data and add network request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Travel Tracker';
-  travelerName = 'traveler';
+  traveler = {
+    id: 44,
+    name: 'Marijo MacNeilley',
+    travelerType: 'photographer'
+  };
+  trips = [];
   spend = '$0.00';
+
+  ngOnInit() {
+    fetch('http://localhost:3001/api/v1/trips').then(response => response.json()).then(data => this.trips = data)
+  }
 }
